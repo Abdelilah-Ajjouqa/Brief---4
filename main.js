9cb04d65228473a64297053085be542c30068585
Abdelilah <abdouajjouqa@gmail.com>
2024-10-31 20:46:41 +0100

    i add a button for back from "add new task"

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const btnAjouter = document.getElementById("addNew");
+const btnAjouter = document.getElementById("taskAdd");
+const btnBack = document.getElementById("taskRemove");
 
 btnAjouter.addEventListener("click", () => {
-    document.getElementById("taskAdd").classList.remove("hidden");
-});
+    document.getElementById("task").classList.remove("hidden");
+    // document.querySelectorAll("#principal").classList.add("backdrop-blur-sm");
+})
+
+btnBack.addEventListener("click", () => {
+    document.getElementById("task").classList.add("hidden");
+})
