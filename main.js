600405d526c61181f0fcee63a7f1532d39440428
Abdelilah <abdouajjouqa@gmail.com>
2024-10-31 21:23:21 +0100

    .

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const btnBack = document.getElementById("taskRemove");
 
 btnAjouter.addEventListener("click", () => {
     document.getElementById("task").classList.remove("hidden");
-    // document.querySelectorAll("#principal").classList.add("backdrop-blur-sm");
 })
 
 btnBack.addEventListener("click", () => {
