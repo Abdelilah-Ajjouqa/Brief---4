8abba5ff3b797a566c750461626146d91d06b452
Abdelilah <abdouajjouqa@gmail.com>
2024-11-03 15:55:47 +0100

    add action to new-task and affiche the data that added from user

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const btnAjouter = document.getElementById("taskAdd");
 const btnBack = document.getElementById("taskRemove");
 const btnDelete = document.querySelectorAll("#taskDelete");
-const btnSave = document.getElementById("savebtn");
-
+const ajtForm = document.getElementById("ajtForm");
 
 const newTitle = document.getElementById("title");
 const newDescription = document.getElementById("description");
@@ -10,28 +9,98 @@ const newStatus = document.getElementById("status");
 const newTypes = document.getElementById("type");
 const newDate = document.getElementById("date");
 
+const todoSection = document.getElementById("todoSection");
+const doingSection = document.getElementById("doingSection");
+const doneSection = document.getElementById("doneSection");
+
 let tasks = [];
 
 // open l'affichage d'ajouter et l'affichage back
 btnAjouter.addEventListener("click", () => {
-    document.getElementById("newTask").classList.remove("hidden");
-})
+  document.getElementById("newTask").classList.remove("hidden");
+});
 
 btnBack.addEventListener("click", () => {
-    document.getElementById("newTask").classList.add("hidden");
-})
+  document.getElementById("newTask").classList.add("hidden");
+});
 
+function resetForm() {
+  newTitle.value = "";
+  newDescription.value = "";
+  newStatus.value = "todo";
+  newTypes.value = "p-1";
+  newDate.value = "";
+}
 
-// tmie to add a place where the infos goes 
-btnSave.addEventListener("click", () => {
-    let info = {
-        Title : newTitle.value,
-        Description : newDescription.value,
-        Status : newStatus.value,
-        Type : newTypes.value,
-        Date : newDate.value,
-    };
-    tasks.push(info)
-    console.log(info);
-    console.log(tasks);
-})
+// times to add a place where the infos goes
+ajtForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (
+    !newTitle.value ||
+    !newDescription.value ||
+    !newStatus.value ||
+    !newTypes.value ||
+    !newDate.value
+  ) {
+    alert("please fill all the fields");
+    return;
+  }
+  let info = {
+    id: Date.now(),
+    Title: newTitle.value,
+    Description: newDescription.value,
+    Status: newStatus.value,
+    Type: newTypes.value,
+    Date: newDate.value,
+  };
+
+  console.log(info);
+
+  tasks.push(info);
+  displayTask(info);
+  resetForm();
+
+  document.getElementById("newTask").classList.add("hidden");
+});
+
+function displayTask(task) {
+
+    let targetSection;
+    let background;
+
+    if (task.Type === "p-1") {
+        background = "bg-green-500"
+    } else if (task.Type === "p-2") {
+        background = "bg-orange-500"
+    } else if (task.Type === "p-3") {
+        background = "bg-red-500"
+    }
+
+    if (task.Status === "todo") {
+        targetSection = document.getElementById("todoSection");
+    } else if (task.Status === "inprogress") {
+        targetSection = document.getElementById("doingSection");
+    } else if (task.Status === "done") {
+        targetSection = document.getElementById("doneSection");
+    }
+
+    const taskCard = document.createElement("div");
+    taskCard.className = ` my-3 text-lg mx-auto w-11/12 ${task.Type} rounded-md ${background}`;
+    taskCard.innerHTML = `
+        <li class="ml-2 text-start text-2xl">${task.Title}</li>
+        <p>${task.Description}</p>  
+        <p>${task.Date}</p>
+        <div class="flex justify-evenly">
+            <button><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="taskDelete"><i class="fa-solid fa-trash"></i></button>
+        </div>
+    `;
+
+    targetSection.appendChild(taskCard);
+
+    taskCard.querySelector(".taskDelete").addEventListener("click", () => {
+        targetSection.removeChild(taskCard);
+        tasks = tasks.filter(t => t !== task);
+    });
+}
